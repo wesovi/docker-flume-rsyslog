372a0ae4375f9e697e951ac09f57c7a66c1f4af8
Iván Corrales <icorrales@paradigmatecnologico.com>
2015-04-07 11:46:51 +0200

    Commenting out global agent

--- bin/run-agents.sh
+++ bin/run-agents.sh
@@ -3,13 +3,13 @@
 sed -i  -e "s/{ES_PATH}/"${ELASTICSEARCH_9300_SERVICE_HOST}"/g" /opt/flume/agent/flume-ripple-flow-agent.properties
 sed -i  -e "s/{ES_PATH}/"${ELASTICSEARCH_9300_SERVICE_HOST}"/g" /opt/flume/agent/flume-global-agent.properties
 
-/opt/flume/bin/flume-ng agent --conf /opt/flume/conf \
-        --conf-file /opt/flume/agent/flume-global-agent.properties \
-        -Dflume.root.logger=DEBUG,LOGFILE \
-        -Dflume.log.dir=/var/flume/logs \
-        -Dflume.log.file=flume-global.log  \
-        --name globalAgent & \
-        -DES_PATH=${ELASTICSEARCH_9300_SERVICE_HOST}:9300 &
+#/opt/flume/bin/flume-ng agent --conf /opt/flume/conf \
+#        --conf-file /opt/flume/agent/flume-global-agent.properties \
+#        -Dflume.root.logger=DEBUG,LOGFILE \
+#        -Dflume.log.dir=/var/flume/logs \
+#        -Dflume.log.file=flume-global.log  \
+#        --name globalAgent & \
+#        -DES_PATH=${ELASTICSEARCH_9300_SERVICE_HOST}:9300 &
 
 /opt/flume/bin/flume-ng agent --conf /opt/flume/conf \
         --conf-file /opt/flume/agent/flume-ripple-flow-agent.properties \
